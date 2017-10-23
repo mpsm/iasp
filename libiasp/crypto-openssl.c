ccb6eacb603f4ef2627e94511d6932eeee3b316f
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2017-10-23 22:05:55 +0200

    calculating identity - work in progress

--- libiasp/crypto-openssl.c
+++ libiasp/crypto-openssl.c
@@ -5,6 +5,7 @@
 #include <openssl/bn.h>
 #include <openssl/ec.h>
 #include <openssl/objects.h>
+#include <openssl/evp.h>
 
 #include <assert.h>
 #include <stdbool.h>
@@ -13,13 +14,16 @@
 
 
 /* private constants */
-static const struct {
+typedef struct {
     iasp_spn_code_t spn_code;
-    int nid;
-} spn_map[] = {
-        {IASP_SPN_128, NID_X9_62_prime256v1},
-        {IASP_SPN_256, NID_secp521r1},
-        {IASP_SPN_MAX, 0},
+    int nid_ec;
+    int nid_dgst;
+    const EVP_MD *md;
+} crypto_context_t;
+static const crypto_context_t spn_map[] = {
+        {IASP_SPN_128, NID_X9_62_prime256v1, NID_sha256},
+        {IASP_SPN_256, NID_secp521r1, NID_sha512},
+        {IASP_SPN_MAX, 0, 0},
 };
 
 /* private variables */
@@ -28,13 +32,15 @@ static iasp_spn_code_t spn = IASP_SPN_MAX;
 static iasp_identity_t my_id;
 
 /* private methods */
-static iasp_identity_t crypto_eckey2id(EC_KEY *key);
-
+//static bool crypto_eckey2id(iasp_spn_code_t spn, EC_KEY *key);
+//static const crypto_context_t *crypto_get_context(iasp_spn_code_t spn);
 
 bool crypto_init(binbuf_t * const pkey)
 {
     const EC_GROUP *group;
     const EC_POINT *pubkey;
+    const EC_POINT *g;
+    BIGNUM *gbn;
     int group_nid;
     int i = 0;
 
@@ -46,10 +52,13 @@ bool crypto_init(binbuf_t * const pkey)
 
     group = EC_KEY_get0_group(my_key);
     group_nid = EC_GROUP_get_curve_name(group);
+    g = EC_GROUP_get0_generator(group);
+    gbn = EC_POINT_point2bn(group, g, POINT_CONVERSION_UNCOMPRESSED, NULL, NULL);
     printf("Curve: %s (%d)\n",  OBJ_nid2ln(group_nid), group_nid);
+    printf("Size: %u (%u bits)\n", BN_num_bytes(gbn), BN_num_bits(gbn));
 
     while(spn_map[i].spn_code != IASP_SPN_MAX) {
-        if(spn_map[i].nid == group_nid) {
+        if(spn_map[i].nid_ec == group_nid) {
             spn = spn_map[i].spn_code;
         }
         i++;
@@ -66,7 +75,7 @@ bool crypto_init(binbuf_t * const pkey)
     printf("Public key (compressed):   %s\n", EC_POINT_point2hex(group, pubkey, POINT_CONVERSION_COMPRESSED, NULL));
     printf("Public key (uncompressed): %s\n", EC_POINT_point2hex(group, pubkey, POINT_CONVERSION_UNCOMPRESSED, NULL));
 
-    my_id = crypto_eckey2id(my_key);
+    //my_id = crypto_eckey2id(my_key);
 
     return true;
 }
@@ -85,19 +94,50 @@ void crypto_free()
     }
 }
 
-
-static iasp_identity_t crypto_eckey2id(EC_KEY *key)
+#if 0
+static const crypto_context_t *crypto_get_context(iasp_spn_code_t spn)
 {
+    const crypto_context_t *ctx = NULL;
+    int i = 0;
+
+    while(spn_map[i].spn_code != IASP_SPN_MAX) {
+        if(spn_map[i].spn_code == spn) {
+            return &spn_map[i];
+        }
+        i++;
+    }
+
+    return ctx;
+}
+
+
+static bool crypto_eckey2id(iasp_spn_code_t spn, EC_KEY *key)
+{
+    const EVP_MD *md;
     const EC_GROUP *group;
     const EC_POINT *pubkey;
+    BIGNUM *n;
     uint8_t *buf;
     size_t buflen;
+    const crypto_context_t *ctx;
 
     assert(key != NULL);
 
-    group = EC_KEY_get0_group(my_key);
-    pubkey = EC_KEY_get0_public_key(my_key);
+    /* get crypto handlers */
+    ctx = crypto_get_context(spn);
+    if(ctx == NULL) {
+        return false;
+    }
+    md = EVP_get_digestbynid(ctx->nid_dgst);
 
+    /* read public key */
+    group = EC_KEY_get0_group(my_key);
+    EC_GROUP_get_order(group, n);
+
+    pubkey = EC_KEY_get0_public_key(my_key);
     EC_POINT_point2oct(group, pubkey, POINT_CONVERSION_UNCOMPRESSED, buf, buflen, NULL);
 
+
+    return true;
 }
+#endif
