3b04c8aecfdbe018ed655c43aa773cb5a8e09b5d
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2017-10-23 08:15:46 +0200

    reading public key

--- libiasp/crypto-openssl.c
+++ libiasp/crypto-openssl.c
@@ -1,6 +1,7 @@
 #include "crypto.h"
 #include "binbuf.h"
 
+#include <openssl/bn.h>
 #include <openssl/ec.h>
 #include <openssl/objects.h>
 
@@ -16,6 +17,7 @@ static EC_KEY *my_key = NULL;
 bool crypto_init(binbuf_t * const pkey)
 {
     const EC_GROUP *group;
+    const EC_POINT *pubkey;
     int group_nid;
 
     assert(pkey != NULL);
@@ -27,6 +29,10 @@ bool crypto_init(binbuf_t * const pkey)
     group = EC_KEY_get0_group(my_key);
     group_nid = EC_GROUP_get_curve_name(group);
     printf("Curve: %s (%d)\n",  OBJ_nid2ln(group_nid), group_nid);
+
+    pubkey = EC_KEY_get0_public_key(my_key);
+    printf("Public key (compressed):   %s\n", EC_POINT_point2hex(group, pubkey, POINT_CONVERSION_COMPRESSED, NULL));
+    printf("Public key (uncompressed): %s\n", EC_POINT_point2hex(group, pubkey, POINT_CONVERSION_UNCOMPRESSED, NULL));
 
     return true;
 }
